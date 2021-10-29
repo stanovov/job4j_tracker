664e357a20c7be9d58016288e199ec3003ea58d0
Semyon Stanovov <sestanovov@gmail.com>
2021-10-29 18:26:39 +0300

    add StartUI

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,38 @@
+package ru.job4j.tracker;
+
+import ru.job4j.tracker.action.UserAction;
+import ru.job4j.tracker.input.Input;
+import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.store.Store;
+
+import java.util.List;
+
+public class StartUI {
+
+    private final Output out;
+
+    public StartUI(Output out) {
+        this.out = out;
+    }
+
+    public void init(Input input, Store tracker, List<UserAction> actions) {
+        boolean run = true;
+        while (run) {
+            showMenu(actions);
+            int select = input.askInt("Select:");
+            if (select < 0 || select >= actions.size()) {
+                out.println("Wrong input, you can select: 0 .. " + (actions.size() - 1));
+                continue;
+            }
+            UserAction action = actions.get(select);
+            run = action.execute(input, tracker);
+        }
+    }
+
+    private void showMenu(List<UserAction> actions) {
+        out.println("Menu.");
+        for (int index = 0; index < actions.size(); index++) {
+            out.println(index + ". " + actions.get(index).name());
+        }
+    }
+}
