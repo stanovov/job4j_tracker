4dc8dcab8ab2f972f69ca337d234704cf962581c
Semyon Stanovov <sestanovov@gmail.com>
2021-10-29 18:45:13 +0300

    application entry point

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,10 +1,16 @@
 package ru.job4j.tracker;
 
-import ru.job4j.tracker.action.UserAction;
+import ru.job4j.tracker.action.*;
+import ru.job4j.tracker.input.ConsoleInput;
 import ru.job4j.tracker.input.Input;
+import ru.job4j.tracker.input.ValidateInput;
+import ru.job4j.tracker.output.ConsoleOutput;
 import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.store.HbmTracker;
 import ru.job4j.tracker.store.Store;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StartUI {
@@ -33,6 +39,25 @@ public class StartUI {
         out.println("Menu.");
         for (int index = 0; index < actions.size(); index++) {
             out.println(index + ". " + actions.get(index).name());
+        }
+    }
+
+    public static void main(String[] args) {
+        Output output = new ConsoleOutput();
+        Input input = new ValidateInput(output, new ConsoleInput());
+        try (Store tracker = new HbmTracker()) {
+            tracker.init();
+            List<UserAction> actions = new ArrayList<>(
+                    Arrays.asList(
+                            new CreateAction(output), new ShowAllAction(output),
+                            new ReplaceAction(output), new DeleteAction(output),
+                            new FindByIdAction(output), new FindByNameAction(output),
+                            new ExitAction()
+                    )
+            );
+            new StartUI(output).init(input, tracker, actions);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
