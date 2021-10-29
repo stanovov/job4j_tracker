a455ced090c4f0a2093c3716fec4a0a3747d776d
Semyon Stanovov <sestanovov@gmail.com>
2021-10-29 17:55:48 +0300

    add MemTracker

---
+++ src/main/java/ru/job4j/tracker/store/MemTracker.java
@@ -0,0 +1,76 @@
+package ru.job4j.tracker.store;
+
+import ru.job4j.tracker.model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemTracker implements Store {
+
+    private final List<Item> items = new ArrayList<>();
+
+    private int ids = 1;
+
+    @Override
+    public void init() {
+    }
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items.add(item);
+        return item;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            item.setId(id);
+            items.set(index, item);
+        }
+        return rsl;
+    }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            items.remove(index);
+        }
+        return rsl;
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(items);
+    }
+
+    public List<Item> findByName(String key) {
+        List<Item> itemsByKey = new ArrayList<>();
+        for (Item item : items) {
+            if (key.equals(item.getName())) {
+                itemsByKey.add(item);
+            }
+        }
+        return itemsByKey;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items.get(index) : null;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < items.size(); index++) {
+            if (items.get(index).getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public void close() throws Exception {
+    }
+}
